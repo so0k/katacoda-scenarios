708fd08575af236dd9f9ecb633035e27360f2850
swo0k <vincentdesmet@swatmobile.io>
2019-03-08 12:56:03 +0700

    [concourse] 02-tasks - fix env-init

--- concourse/02-tasks/env-init.sh
+++ concourse/02-tasks/env-init.sh
@@ -1,21 +1,11 @@
-echo "Updating curl"
-apt-get update
-apt-get install curl 
-
 echo "Standing up concourse playground"
 docker-compose up -d
 
-echo "Waiting for concourse server"
-curl --connect-timeout 5 \
-  --max-time 10 \
-  --retry 5 \
-  --retry-connrefuse \
-  --retry-delay 0 \
-  --retry-max-time 40  \
-  -so fly http://docker:8080/api/v1/cli?arch=amd64&platform=linux
-
-chmod +x fly
+curl -Lo fly.tar.gz https://github.com/concourse/concourse/releases/download/v5.0.0/fly-5.0.0-linux-amd64.tgz
+tar -xzf fly.tar.gz && rm fly.tar.gz
 mv fly /usr/local/bin
+
+while ! curl -sI http://docker:8080 >/dev/null 2>&1; do echo "Waiting for concourse to be online"; sleep 1; done
 
 echo "Configuring concourse client"
 set -x
